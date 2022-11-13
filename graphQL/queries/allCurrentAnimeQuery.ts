bc80dcf43a44cbe435c81adc5545047a151c2bdf
kyle.bautista <bautista.kvb@gmail.com>
2022-11-13 02:01:23 -0800

    add sort filter

--- graphQL/queries/allCurrentAnimeQuery.ts
+++ graphQL/queries/allCurrentAnimeQuery.ts
@@ -2,8 +2,143 @@ import { gql } from "graphql-request";
 
 export default gql`
   {
-    Page(page: 1, perPage: 50) {
+    fall: Page(page: 1, perPage: 50) {
       media(season: FALL, seasonYear: 2022, sort: POPULARITY_DESC) {
+        description
+        coverImage {
+          extraLarge
+          large
+          medium
+          color
+        }
+        idMal
+        title {
+          romaji
+          english
+          native
+        }
+        studios(isMain: true) {
+          nodes {
+            name
+          }
+        }
+        startDate {
+          year
+          month
+          day
+        }
+        status
+        episodes
+        duration
+        source
+        genres
+        averageScore
+        upcomingEpisode: nextAiringEpisode {
+          id
+          episode
+          timeUntilAiring
+          mediaId
+        }
+        firstEpisode: airingSchedule(notYetAired: false, page: 1, perPage: 1) {
+          episode: nodes {
+            airingAt
+            episode
+          }
+        }
+      }
+    }
+    summer: Page(page: 1, perPage: 50) {
+      media(season: SUMMER, seasonYear: 2022, sort: POPULARITY_DESC) {
+        description
+        coverImage {
+          extraLarge
+          large
+          medium
+          color
+        }
+        idMal
+        title {
+          romaji
+          english
+          native
+        }
+        studios(isMain: true) {
+          nodes {
+            name
+          }
+        }
+        startDate {
+          year
+          month
+          day
+        }
+        status
+        episodes
+        duration
+        source
+        genres
+        averageScore
+        upcomingEpisode: nextAiringEpisode {
+          id
+          episode
+          timeUntilAiring
+          mediaId
+        }
+        firstEpisode: airingSchedule(notYetAired: false, page: 1, perPage: 1) {
+          episode: nodes {
+            airingAt
+            episode
+          }
+        }
+      }
+    }
+    spring: Page(page: 1, perPage: 50) {
+      media(season: SPRING, seasonYear: 2022, sort: POPULARITY_DESC) {
+        description
+        coverImage {
+          extraLarge
+          large
+          medium
+          color
+        }
+        idMal
+        title {
+          romaji
+          english
+          native
+        }
+        studios(isMain: true) {
+          nodes {
+            name
+          }
+        }
+        startDate {
+          year
+          month
+          day
+        }
+        status
+        episodes
+        duration
+        source
+        genres
+        averageScore
+        upcomingEpisode: nextAiringEpisode {
+          id
+          episode
+          timeUntilAiring
+          mediaId
+        }
+        firstEpisode: airingSchedule(notYetAired: false, page: 1, perPage: 1) {
+          episode: nodes {
+            airingAt
+            episode
+          }
+        }
+      }
+    }
+    winter: Page(page: 1, perPage: 50) {
+      media(season: WINTER, seasonYear: 2022, sort: POPULARITY_DESC) {
         description
         coverImage {
           extraLarge
