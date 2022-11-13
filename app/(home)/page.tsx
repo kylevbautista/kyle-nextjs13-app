6960c7a066dfda8c324f6edff5b104669c909e4d
kyle.bautista <bautista.kvb@gmail.com>
2022-11-13 12:10:45 -0800

    use client

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import PageBase from "./components/PageBase";
 import { getSeasonNow } from "../jinkan/seasonNow";
 import { request } from "graphql-request";
@@ -5,15 +6,17 @@ import { graphQLClient } from "../../graphQL/graphqlClient";
 import allCurrentAnimeQuery from "../../graphQL/queries/allCurrentAnimeQuery";
 import Button from "./components/Button";
 
-export const dynamic = "force-dynamic",
-  dynamicParams = true,
-  revalidate = false,
-  fetchCache = "auto",
-  runtime = "nodejs",
-  preferredRegion = "auto";
+// export const dynamic = "force-dynamic",
+//   dynamicParams = true,
+//   revalidate = false,
+//   fetchCache = "auto",
+//   runtime = "nodejs",
+//   preferredRegion = "auto";
 
 export default async function Home() {
   const { data, errors, extensions, headers, status } =
     await graphQLClient.rawRequest(allCurrentAnimeQuery);
+  console.log("data", data);
+  console.log("headers", headers);
   return <PageBase data={data} />;
 }
