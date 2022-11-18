8efb1f13a12d76dc7053affc055013666e08ad30
kyle.bautista <bautista.kvb@gmail.com>
2022-11-18 01:56:10 -0800

    restructure 1123

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,40 @@
 import PageBase from "./components/PageBase";
 import { graphQLClient } from "../graphQL/graphqlClient";
 import allCurrentAnimeQueryFetch from "../graphQL/queries/allCurrentAnimeQueryFetch";
+import { redirect } from "next/navigation";
 
 /**
  * Can't invalidate cache in nextjs13 with graphqlrequest
  * Have to use native fetch api to make graphql post request
  * @returns data
  */
-const getData = async () => {
-  try {
-    const dateObject = new Date();
-    const year = dateObject.getUTCFullYear();
-    const res = await fetch("https://graphql.anilist.co", {
-      next: { revalidate: 30 },
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        query: allCurrentAnimeQueryFetch,
-        variables: {
-          year: year,
-        },
-      }),
-    });
-    const data = await res.json();
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-};
+// const getData = async () => {
+//   try {
+//     const dateObject = new Date();
+//     const year = dateObject.getUTCFullYear();
+//     const res = await fetch("https://graphql.anilist.co", {
+//       next: { revalidate: 30 },
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         query: allCurrentAnimeQueryFetch,
+//         variables: {
+//           year: year,
+//         },
+//       }),
+//     });
+//     const data = await res.json();
+//     return data;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
-export default async function Home() {
-  const { data } = (await getData()) || {};
-  return <PageBase data={data} />;
+export default function Home() {
+  const dateObject = new Date();
+  const currentYear = dateObject.getUTCFullYear();
+  redirect(`/${currentYear}`);
+  return null;
 }
