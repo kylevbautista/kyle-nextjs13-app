6a016bd046cc3ae8d7fc7ba2dc7d928119066c3a
kyle.bautista <bautista.kvb@gmail.com>
2022-12-08 14:47:26 -0800

    coarse

--- components/anime/year/AnimeInfoGrid.tsx
+++ components/anime/year/AnimeInfoGrid.tsx
@@ -239,7 +239,7 @@ export default function AnimeInfoGrid({
           <div
             id={`anime-snopsis-${1}`}
             onTouchStart={() => {}}
-            className="scrollbar border-b pl-1 pr-1 overflow-y-hidden hover:overflow-auto active:overflow-auto focus:overflow-auto border-inherit"
+            className="scrollbar border-b pl-1 pr-1 overflow-y-hidden hover:overflow-auto border-inherit"
           >
             {description ? (
               <div className="text-xs">
