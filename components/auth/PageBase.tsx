209bb5f499d51191bf738d20d01c4fb0cead3a53
kyle.bautista <bautista.kvb@gmail.com>
2022-12-22 22:11:51 -0800

    rebuild

--- components/auth/PageBase.tsx
+++ components/auth/PageBase.tsx
@@ -9,7 +9,7 @@ interface PageBaseProps {
 export default function PageBase({ children }: PageBaseProps) {
   const { data: session } = useSession();
   // console.log("kylelog session: ", session);
-  // console.log("Kylelog env:", process.env.NODE_ENV);
+  console.log("Kylelog env:", process.env.NODE_ENV);
   return (
     <div
       id="container"
