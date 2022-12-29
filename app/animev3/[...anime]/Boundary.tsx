c542895d1543c999b2f3bcb8c2e4cb1d74499e04
kyle.bautista <bautista.kvb@gmail.com>
2022-12-29 00:08:31 -0800

    rebuild

--- app/animev3/[...anime]/Boundary.tsx
+++ app/animev3/[...anime]/Boundary.tsx
@@ -11,13 +11,18 @@ interface PageBaseProps {
 }
 
 export default async function Boundary({ year, season }: PageBaseProps) {
-  const { data = {} } =
-    (await getAniListDataByYear({
-      year: year,
-      timeout: 5000,
-      enableLogs: false,
-    })) || {};
+  // const { data = {} } =
+  //   (await getAniListDataByYear({
+  //     year: year,
+  //     timeout: 5000,
+  //     enableLogs: false,
+  //   })) || {};
+  const data = await getAniListDataByYear({
+    year: year,
+    timeout: 5000,
+    enableLogs: false,
+  });
   const obj = { year: year, season: season };
 
-  return <PageBase year={year} params={obj} data={data} />;
+  return <PageBase year={year} params={obj} data={data?.data} />;
 }
