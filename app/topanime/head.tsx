31d873d783b395c65d9360d76016255e23c4f77d
kyle.bautista <bautista.kvb@gmail.com>
2023-04-09 18:14:06 -0700

    remove nextjs 13.3 deprecated files

--- app/topanime/head.tsx
+++
@@ -1,8 +0,0 @@
-export default function Head({ params }: any) {
-  return (
-    <>
-      <title>{`Top Anime`}</title>
-      <meta name="description" content={`Top Anime`}></meta>
-    </>
-  );
-}
