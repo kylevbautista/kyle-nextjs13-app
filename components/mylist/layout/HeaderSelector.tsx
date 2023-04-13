7c32ff684def350b269f87c0cf9c15bf46d11f59
kyle.bautista <bautista.kvb@gmail.com>
2023-04-13 02:06:43 -0700

    041323-mylist-options

--- components/mylist/layout/HeaderSelector.tsx
+++ components/mylist/layout/HeaderSelector.tsx
@@ -4,66 +4,104 @@ import { HeaderContext } from "./HeaderProvider";
 
 export function HeaderSelector() {
   const header = useContext(HeaderContext);
-  const { option, setOption } = header;
+  const { option, setOption, season, setSeason, day, setDay } = header;
   const selected =
     "flex items-center h-[44px] border-b border-blue-500 cursor-pointer text-blue-500 font-bold hover:border-blue-500";
   const unSelected =
     "flex items-center h-[44px] border-b border-transparent cursor-pointer text-[rgb(164,164,164)] hover:border-blue-500";
 
   return (
-    <div className="text-white flex flex-wrap justify-center items-end gap-5">
+    <div className="text-white flex flex-wrap flex-col items-center justify-end">
       <div className="flex justify-center items-center gap-5 basis-full sm:flex-initial">
         <button
-          className={option === "Any" ? selected : unSelected}
-          onClick={() => setOption("Any")}
+          className={season == "winter" ? selected : unSelected}
+          onClick={() => {
+            setSeason("winter");
+          }}
+        >
+          <p className="hidden sm:block">Winter</p>
+          <p className="sm:hidden">Win</p>
+        </button>
+        <button
+          className={season === "spring" ? selected : unSelected}
+          onClick={() => {
+            setSeason("spring");
+          }}
+        >
+          <p className="hidden sm:block">Spring</p>
+          <p className="sm:hidden">Spr</p>
+        </button>
+        <button
+          className={season === "summer" ? selected : unSelected}
+          onClick={() => {
+            setSeason("summer");
+          }}
+        >
+          <p className="hidden sm:block">Summer</p>
+          <p className="sm:hidden">Sum</p>
+        </button>
+        <button
+          className={season === "fall" ? selected : unSelected}
+          onClick={() => {
+            setSeason("fall");
+          }}
+        >
+          <p className="hidden sm:block">Fall</p>
+          <p className="sm:hidden">Fall</p>
+        </button>
+      </div>
+      <div className="flex justify-center items-center gap-5 basis-full sm:flex-initial">
+        <button
+          className={day === "any" ? selected : unSelected}
+          onClick={() => setDay("any")}
         >
           <p className="">All</p>
         </button>
         <button
-          className={option === "Monday" ? selected : unSelected}
-          onClick={() => setOption("Monday")}
+          className={day === "monday" ? selected : unSelected}
+          onClick={() => setDay("monday")}
         >
           <p className="hidden sm:block">Monday</p>
           <p className="sm:hidden">Mon</p>
         </button>
         <button
-          className={option === "Tuesday" ? selected : unSelected}
-          onClick={() => setOption("Tuesday")}
+          className={day === "tuesday" ? selected : unSelected}
+          onClick={() => setDay("tuesday")}
         >
           <p className="hidden sm:block">Tuesday</p>
           <p className="sm:hidden">Tues</p>
         </button>
         <button
-          className={option === "Wednesday" ? selected : unSelected}
-          onClick={() => setOption("Wednesday")}
+          className={day === "wednesday" ? selected : unSelected}
+          onClick={() => setDay("wednesday")}
         >
           <p className="hidden sm:block">Wednesday</p>
           <p className="sm:hidden">Wed</p>
         </button>
         <button
-          className={option === "Thursday" ? selected : unSelected}
-          onClick={() => setOption("Thursday")}
+          className={day === "thursday" ? selected : unSelected}
+          onClick={() => setDay("thursday")}
         >
           <p className="hidden sm:block">Thursday</p>
           <p className="sm:hidden">Thu</p>
         </button>
         <button
-          className={option === "Friday" ? selected : unSelected}
-          onClick={() => setOption("Friday")}
+          className={day === "friday" ? selected : unSelected}
+          onClick={() => setDay("friday")}
         >
           <p className="hidden sm:block">Friday</p>
           <p className="sm:hidden">Fri</p>
         </button>
         <button
-          className={option === "Saturday" ? selected : unSelected}
-          onClick={() => setOption("Saturday")}
+          className={day === "saturday" ? selected : unSelected}
+          onClick={() => setDay("saturday")}
         >
           <p className="hidden sm:block">Saturday</p>
           <p className="sm:hidden">Sat</p>
         </button>
         <button
-          className={option === "Sunday" ? selected : unSelected}
-          onClick={() => setOption("Sunday")}
+          className={day === "sunday" ? selected : unSelected}
+          onClick={() => setDay("sunday")}
         >
           <p className="hidden sm:block">Sunday</p>
           <p className="sm:hidden">Sun</p>
--- components/mylist/utils/getSortedData.ts
+++ components/mylist/utils/getSortedData.ts
@@ -1,64 +1,57 @@
 import { unixTimeStampToWeekDay } from "../../animev3/utils/timeStampHelpers";
 
+const getSeason = (month: any) => {
+  if (month <= 3) {
+    return "winter";
+  }
+  if (month >= 4 && month <= 6) {
+    return "spring";
+  }
+  if (month >= 7 && month <= 9) {
+    return "summer";
+  }
+  if (month >= 10 && month <= 12) {
+    return "fall";
+  }
+};
+
 export const getSortedData = ({
   option = "Any",
+  yearOption = 2023,
+  seasonOption = "winter",
+  dayOption = "all",
   listSorted = [],
-}: {
-  option: any;
-  listSorted: any;
-}) => {
-  const mondayList = listSorted?.filter(
-    (anime: any) =>
-      unixTimeStampToWeekDay(anime?.firstEpisode?.episode[0]?.airingAt)
-        ?.dayOfTheWeek === "Monday"
-  );
-  const TuesdayList = listSorted?.filter(
-    (anime: any) =>
-      unixTimeStampToWeekDay(anime?.firstEpisode?.episode[0]?.airingAt)
-        ?.dayOfTheWeek === "Tuesday"
-  );
-  const WednesdayList = listSorted?.filter(
-    (anime: any) =>
-      unixTimeStampToWeekDay(anime?.firstEpisode?.episode[0]?.airingAt)
-        ?.dayOfTheWeek === "Wednesday"
-  );
-  const ThursdayList = listSorted?.filter(
-    (anime: any) =>
-      unixTimeStampToWeekDay(anime?.firstEpisode?.episode[0]?.airingAt)
-        ?.dayOfTheWeek === "Thursday"
-  );
-  const FridayList = listSorted?.filter(
-    (anime: any) =>
-      unixTimeStampToWeekDay(anime?.firstEpisode?.episode[0]?.airingAt)
-        ?.dayOfTheWeek === "Friday"
-  );
-  const SaturdayList = listSorted?.filter(
-    (anime: any) =>
-      unixTimeStampToWeekDay(anime?.firstEpisode?.episode[0]?.airingAt)
-        ?.dayOfTheWeek === "Saturday"
-  );
-  const SundayList = listSorted?.filter(
-    (anime: any) =>
-      unixTimeStampToWeekDay(anime?.firstEpisode?.episode[0]?.airingAt)
-        ?.dayOfTheWeek === "Sunday"
-  );
-  let data = listSorted;
-  if (option === "Monday") {
-    data = mondayList;
-  } else if (option === "Tuesday") {
-    data = TuesdayList;
-  } else if (option === "Wednesday") {
-    data = WednesdayList;
-  } else if (option === "Thursday") {
-    data = ThursdayList;
-  } else if (option === "Friday") {
-    data = FridayList;
-  } else if (option === "Saturday") {
-    data = SaturdayList;
-  } else if (option === "Sunday") {
-    data = SundayList;
-  }
+}: any) => {
+  const SeasonDayList = listSorted?.filter((anime: any) => {
+    const startDate = anime?.startDate || {};
+    const { day = 1, month = 1, year = 1 } = startDate;
 
+    if (
+      getSeason(month) === seasonOption &&
+      yearOption == year &&
+      dayOption ==
+        unixTimeStampToWeekDay(
+          anime?.firstEpisode?.episode[0]?.airingAt
+        )?.dayOfTheWeek.toLowerCase()
+    ) {
+      return anime;
+    }
+  });
+  const SeasonAllList = listSorted?.filter((anime: any) => {
+    const startDate = anime?.startDate || {};
+    const { day = 1, month = 1, year = 1 } = startDate;
+
+    if (getSeason(month) === seasonOption && yearOption == year) {
+      return anime;
+    }
+  });
+
+  let data = listSorted;
+  if (dayOption == "any") {
+    data = SeasonAllList;
+  } else {
+    data = SeasonDayList;
+  }
   return {
     data,
   };
