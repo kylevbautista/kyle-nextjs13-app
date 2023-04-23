429392baeefa7df887dbd1a5f56ee20548a39185
kyle.bautista <bautista.kvb@gmail.com>
2023-04-23 00:48:58 -0700

    move execution to edge

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   swcMinify: true,
   experimental: {
     appDir: true,
+    runtime: "experimental-edge",
     optimizeCss: true,
     /**
      * To stop rate limiting, I am forcing nextjs
