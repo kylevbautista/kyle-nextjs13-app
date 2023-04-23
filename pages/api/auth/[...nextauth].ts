655890fd00b65039be42eb9813cb696b1ea544c7
kyle.bautista <bautista.kvb@gmail.com>
2023-04-23 12:14:35 -0700

    test

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,3 +1,3 @@
 import authNextHandler from "../../../server/auth";
-
+export const runtime = "edge"; // 'nodejs' is the default
 export default authNextHandler;
--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,3 +1,3 @@
 import graphqlNextHandler from "../../server/graphql";
-export const runtime = "edge"; // 'nodejs' is the default
+
 export default graphqlNextHandler;
