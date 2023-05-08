a77def617928d8109ecd7c298f38fe78e851e102
kyle.bautista <bautista.kvb@gmail.com>
2023-05-07 17:38:59 -0700

    revert if doesn't work

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,42 @@
-import authNextHandler from "@/server/auth";
+import NextAuth from "next-auth";
+import type { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "@/server/lib/mongodb";
 
-export { authNextHandler as GET, authNextHandler as POST };
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      authorization: {
+        params: {
+          prompt: "login",
+        },
+      },
+    }),
+    // ...add more providers here
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+  pages: {
+    signIn: "/auth/signin",
+  },
+  callbacks: {
+    /**
+     * @param  {object} session      Session object
+     * @param  {object} token        User object    (if using database sessions)
+     *                               JSON Web Token (if not using database sessions)
+     * @return {object}              Session that will be returned to the client
+     */
+    async session({ session, user, token }) {
+      // Add property to session, like an access_token from a provider.
+      session.objectId = user.id;
+      return session;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
