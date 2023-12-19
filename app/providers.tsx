3cfae7ad11ad012095ba51d741f419f5c5fb67fc
Kyle Bautista <46615285+kylevbautista@users.noreply.github.com>
2023-12-19 14:35:19 -0800

    121923 | fix deopt due to nprogress usesearchparams (#125)
    121923 | fix deopt due to nprogress usesearchparams (#125)

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,21 +1,20 @@
 "use client";
-import { useCallback } from "react";
 import { HydrationProvider } from "../components/common/HydrationProvider";
 import { SessionProvider } from "next-auth/react";
-import { RouterChangeProviderWrapper } from "../components/common/RouterChangeProviderWrapper";
+// import { RouterChangeProviderWrapper } from "../components/common/RouterChangeProviderWrapper";
 import { Analytics } from "@vercel/analytics/react";
 import { Toaster } from "react-hot-toast";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <RouterChangeProviderWrapper>
-        <HydrationProvider>
-          {children}
-          <Toaster />
-          <Analytics />
-        </HydrationProvider>
-      </RouterChangeProviderWrapper>
+      {/* <RouterChangeProviderWrapper> */}
+      <HydrationProvider>
+        {children}
+        <Toaster />
+        <Analytics />
+      </HydrationProvider>
+      {/* </RouterChangeProviderWrapper> */}
     </SessionProvider>
   );
 }
